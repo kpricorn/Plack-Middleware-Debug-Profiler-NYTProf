53afab16df4e8285f053c22485b5c4c1c4009d18
sebastian de castelberg <github.com@kpricorn.org>
2010-09-29 22:43:18 +0200

    - ENV{NYTPROF}: addpid=1:start=no:file=".$self->root."/nytprof.out - Debug output

--- lib/Plack/Middleware/Debug/Profiler/NYTProf.pm
+++ lib/Plack/Middleware/Debug/Profiler/NYTProf.pm
@@ -3,9 +3,8 @@ use 5.008;
 use strict;
 use warnings;
 
-use Plack::Util::Accessor qw(files root exclude);
+use Plack::Util::Accessor qw(root exclude);
 use Time::HiRes;
-use Devel::NYTProf;
 
 use parent 'Plack::Middleware::Debug::Base';
 our $VERSION = '0.01';
@@ -13,19 +12,30 @@ our $VERSION = '0.01';
 sub prepare_app {
     my $self = shift;
     $self->root($self->root || '/tmp');
-    $self->files(Plack::App::File->new(root => $self->root));
+    $self->{files} = Plack::App::File->new(root => $self->root);
 
-    $self->exclude($self->exclude || [qw(/css.*)]);
+    unless(-d $self->root){
+        mkdir $self->root or die "Cannot create directory " . $self->root;
+    }
+
+    $ENV{NYTPROF} ||= "addpid=1:start=no:file=".$self->root."/nytprof.out";
+    require Devel::NYTProf;
+
+    $self->exclude($self->exclude || [qw(.*.css .*.png .*.ico .*.js)]);
     Carp::croak "exclude not an array" if ref($self->exclude) ne 'ARRAY';
 }
 
 sub call {
     my($self, $env) = @_;
-
+    print STDERR "NYTProf:  ", $env->{PATH_INFO}, "\n";
     if ($env->{PATH_INFO} =~ m!nytprofhtml!) {
+        print STDERR "NYTProf: MATCH \n";
         $env->{'plack.debug.disabled'} = 1;
-        return $self->files->call($env);
+        return $self->{files}->call($env);
     }
+
+    print STDERR "NYTProf: enable_profile \n";
+    DB::enable_profile();
     return $self->SUPER::call($env);
 }
 
@@ -33,42 +43,33 @@ sub run {
     my($self, $env, $panel) = @_;
 
     foreach my $pattern (@{$self->exclude}) {
-        return if $env->{PATH_INFO} =~ m!^$pattern$!;
+        if ($env->{PATH_INFO} =~ m!^$pattern$!) {
+            $panel->nav_subtitle('Excluded');
+            print STDERR "NYTProf: excluded ", $env->{PATH_INFO}, " \n";
+            return;
+        }
     }
-    $self->start($env);
+
     return sub {
         my $res = shift;
-        $self->end($env);
+        print STDERR "NYTProf: finish_profile\n";
+        DB::finish_profile();
         $self->report($env);
+        $panel->nav_subtitle('OK');
 
-        $panel->content('<iframe src ="/nytprofhtml/index.html" width="100%" height="100%">
+        $panel->content('<iframe src ="/nytprofhtml.'.$$.'/index.html" width="100%" height="100%">
           <p>Your browser does not support iframes.</p>
         </iframe>');
     };
 }
 
-sub start {
-    my ( $self, $env ) = @_;
-    my $id = Time::HiRes::gettimeofday;
-    $env->{PROFILE_ID} = $id;
-    DB::enable_profile($self->root . "/nytprof.$id.out");
-}
-
-sub end {
-    DB::disable_profile();
-}
-
 sub report {
     my ( $self, $env ) = @_;
-    if ( my $id = $env->{PROFILE_ID} ) {
-        DB::enable_profile($self->root . "/nyprof.null.out");
-        DB::disable_profile();
-        system "nytprofhtml", "-f", $self->root . "/nytprof.$id.out", "-o", $self->root . "/nytprofhtml";
+    print STDERR "NYTProf: report\n";
+    if ( -f $self->root . "/nytprof.out.$$" ) {
+        print STDERR "NYTProf: nytprofhtml\n";
+        system "nytprofhtml", "-f", $self->root . "/nytprof.out.$$", "-o", $self->root . "/nytprofhtml.$$";
     }
-}
-
-sub DESTROY {
-    DB::finish_profile();
 }
 
 1;
@@ -104,7 +105,7 @@ Where to store nytprof.out and nytprofhtml output (default: '/tmp').
 
 =head2 exclude
 
-List of excluded paths (default: ['/css.*']).
+List of excluded paths (default: ['/css.*', '/favicon.ico']).
 
 =head1 SEE ALSO
 
